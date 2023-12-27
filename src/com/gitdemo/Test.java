4cc9b50aa01aed26931fee4f9ff8cc74e99f5018
Tnanemrep <Tnanemrep@outlook.com>
2023-12-27 19:15:01 +0800

    IDEA commit 01

---
+++ src/com/gitdemo/Test.java
@@ -0,0 +1,14 @@
+package com.gitdemo;
+
+/**
+ * @Description:
+ * @Author: ldk
+ * @Date: 2023/12/27 19:07
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
