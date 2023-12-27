40bf5559581897ba3b8297a0e3a887cbaedd8fa2
Tnanemrep <Tnanemrep@outlook.com>
2023-12-27 19:20:59 +0800

    IDEA commit 03 StackCode Del

--- src/com/gitdemo/Test.java
+++ src/com/gitdemo/Test.java
@@ -15,9 +15,6 @@ public class Test {
         System.out.println(stack.pop());
         stack.push(2);
         stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        stack.push(6);
         System.out.println(stack.pop());
         System.out.println("hello world!");
     }
