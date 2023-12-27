34b6ff67ae1fabc9493d8ae51be71fec8f493ecb
Tnanemrep <Tnanemrep@outlook.com>
2023-12-27 19:17:47 +0800

    IDEA commit 02 StackCode

--- src/com/gitdemo/Test.java
+++ src/com/gitdemo/Test.java
@@ -1,5 +1,7 @@
 package com.gitdemo;
 
+import java.util.Stack;
+
 /**
  * @Description:
  * @Author: ldk
@@ -8,6 +10,15 @@ package com.gitdemo;
 public class Test {
 
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        System.out.println(stack.pop());
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        System.out.println(stack.pop());
         System.out.println("hello world!");
     }
 
